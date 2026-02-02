65188a7301923fa2e3a25948cd30dfc50402567b
pepebolas75 <pepebolas75@gmail.com>
2026-02-02 10:07:16 -0500

    monitoreo

--- src/app/views/home/home.ts
+++ src/app/views/home/home.ts
@@ -1,29 +1,32 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-// Importamos el nuevo componente
 import { AnimatedBackground } from '../../components/animated-background/animated-background';
+import { Product } from '../../services/product';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  // Lo añadimos a los imports
   imports: [CommonModule, RouterLink, AnimatedBackground],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home implements OnInit {
+  private productService = inject(Product);
+
   cargando = true;
-  productos = [
-    { id: 1, nombre: 'RTX 4090 Gaming OC', descripcion: '24GB GDDR6X, Arquitectura Ada Lovelace', precio: 1899, imagen: 'https://m.media-amazon.com/images/I/7122hH4+9JL._AC_SL1500_.jpg' },
-    { id: 2, nombre: 'Intel Core i9-14900K', descripcion: '24 núcleos, hasta 6.0 GHz, Socket LGA1700', precio: 629, imagen: 'https://m.media-amazon.com/images/I/61p6cM8i4ZL._AC_SL1200_.jpg' },
-    { id: 3, nombre: 'Monitor Odyssey G9', descripcion: '49" OLED Curvo, 240Hz, 0.03ms', precio: 1299, imagen: 'https://m.media-amazon.com/images/I/6125mFrzr6L._AC_SL1000_.jpg' },
-    { id: 4, nombre: 'MacBook Pro M3 Max', descripcion: 'Chip M3 Max, 36GB RAM, 1TB SSD', precio: 3199, imagen: 'https://m.media-amazon.com/images/I/618d5bS2lUL._AC_SL1500_.jpg' }
-  ];
+  productos: any[] = [];
 
   ngOnInit() {
-    setTimeout(() => {
-      this.cargando = false;
-    }, 1500);
+    this.productService.getAll().subscribe({
+      next: (data) => {
+        this.productos = data;
+        this.cargando = false;
+      },
+      error: (error) => {
+        console.error('Error al cargar productos:', error);
+        this.cargando = false;
+      }
+    });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  backendBaseUrl: 'https://tech-store-backend.onrender.com'
+  backendBaseUrl: 'http://localhost:8080'
 };
